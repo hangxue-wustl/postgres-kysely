d4155dfb77503faadb6e1992e6ec64f5ce3df3fa
Hang Xue <hang_xue@berkeley.edu>
2024-04-13 14:43:53 -0700

    modify sidenav

--- app/page.tsx
+++ app/page.tsx
@@ -23,26 +23,16 @@ export default function Home() {
       </h1>
       <Suspense fallback={<TablePlaceholder />}>
         <Table />
-
+      </Suspense>
       <div className="flex justify-center space-x-5 pt-10 mt-10 border-t border-gray-300 w-full max-w-xl text-gray-600">
         <Link
-          href="https://postgres-prisma.vercel.app/"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
-        >
-          dashboard
+          href="/dashboard"
+          className="group mt-20 sm:mt-0 rounded-full flex space-x-1 bg-white/30 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all"
+          >
+          <p>Click here more detailed analysis of the survey results!</p>
+          <ExpandingArrow />
         </Link>
-        <Link
-          href="https://postgres-starter.vercel.app/"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
-        >
-          Starter
-        </Link>
-        <Link
-          href="https://postgres-drizzle.vercel.app/"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
-        >
-          Drizzle
-        </Link>
+        
       </div>
 
       <div className="sm:absolute sm:bottom-0 w-full px-20 py-10 flex justify-between">
@@ -69,7 +59,7 @@ export default function Home() {
           <p className="font-light">Source</p>
         </Link>
       </div>
-      </Suspense>
+      
       <p className="font-light text-gray-600 w-full max-w-lg text-center mt-6">
         <Link
           href="https://osmhhelp.org/research.html"

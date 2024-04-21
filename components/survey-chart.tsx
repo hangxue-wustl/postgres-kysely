ccd3c0b2276bf3819e14775b882d7974cf1f3a50
Hang Xue <hang_xue@berkeley.edu>
2024-04-21 12:18:33 -0700

    delete chart

--- components/survey-chart.tsx
+++ components/survey-chart.tsx
@@ -1,6 +1,6 @@
-import { generateYAxis } from '@/lib/utils';
+// import { generateYAxis } from '@/lib/utils';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchSurveyResult } from '@/lib/data';
+// import { fetchSurveyResult } from '@/lib/data';
 
 // This component is representational only.
 // For data visualization UI, check out:
